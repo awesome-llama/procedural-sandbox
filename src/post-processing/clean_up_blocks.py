8a50696114f238b0f3f72db0b06bae24d0585b4e
awesome-llama <17157128+awesome-llama@users.noreply.github.com>
2025-06-01 05:04:37 +1000

    add clean up blocks post process

---
+++ src/post-processing/clean_up_blocks.py
@@ -0,0 +1,112 @@
+GRID_SIZE = 40 # the block grid is 40 units. Reporters are 40 units tall
+MARGIN = 8
+
+SCRIPT_SPACING = GRID_SIZE*2
+
+
+HAT_PRIORITY = [
+    'event_whenflagclicked',
+    'event_whenkeypressed',
+    'event_whenthisspriteclicked',
+    'event_whenbroadcastreceived',
+    'event_whengreaterthan',
+    'control_start_as_clone',
+    'procedures_definition',
+]
+
+
+def clean_target(target):
+    blocks = target['blocks']
+
+    # find all the parent blocks and sort them
+    parents = []
+    for block_id, block in blocks.items():
+        if block['topLevel']:
+            if block['opcode'] in HAT_PRIORITY:
+                parents.append((block_id, HAT_PRIORITY.index(block['opcode'])))
+            else:
+                parents.append((block_id, 1000))
+    parents.sort(key=lambda x: x[1])
+    
+    x = 0
+    y = 0
+
+    for parent in parents:
+        block_id = parent[0]
+        block = blocks[parent[0]]
+        block['x'] = x
+        block['y'] = y
+        #curr_x += 20
+        y += (script_height(blocks, block_id)) + SCRIPT_SPACING
+
+
+def script_height(blocks, parent_id):
+    # recurse though children to find dimensions of each block
+    
+    h = _height_stack(blocks, parent_id)
+    
+    if blocks[parent_id]['opcode'] == 'procedures_definition':
+        h += MARGIN*2 # definition hats are tall
+    
+    # h += 36 # definition
+    # h += 18 # rounded top part of hat
+    
+    return h
+
+
+def _height_stack(blocks, block_id):
+    block = blocks[block_id]
+
+    h = GRID_SIZE + MARGIN # stack height
+
+    if block['opcode'].startswith('pen_'):
+        h += 8 # pen blocks are slightly taller due to the icon
+
+    input_h = 0
+    nested_h = 0
+    for input_name, input in block.get('inputs', {}).items():
+        if input[0] == 2 and input[1] is not None:
+            if input_name == 'SUBSTACK' or input_name == 'SUBSTACK2':
+                # stack
+                nested_h += _height_stack(blocks, input[1])
+                nested_h += 32 # bottom part of C shape
+            else:
+                # boolean
+                input_h = max(input_h, _height_reporter_input(blocks, input[1]) + MARGIN)
+            
+        elif input[0] == 3 and input[1] is not None:
+            # round reporter
+            input_h = max(input_h, _height_reporter_input(blocks, input[1]) + MARGIN)
+    
+    h = max(h, input_h)
+    h += nested_h
+    
+    # next stack block
+    if block['next'] is not None:
+        h += _height_stack(blocks, block['next'])
+    
+    return h
+
+
+
+def _height_reporter_input(blocks, block_id):
+    if block_id is None: 
+        return 0
+    if not isinstance(block_id, str):
+        return GRID_SIZE
+    
+    block = blocks[block_id]
+
+    h = GRID_SIZE
+    for input_name, input in block.get('inputs', {}).items():
+        if input[0] == 2 and input[1] is not None:
+            # boolean
+            h = max(h, _height_reporter_input(blocks, input[1]) + MARGIN)
+            
+        elif input[0] == 3 and input[1] is not None:
+            # round reporter
+            h = max(h, _height_reporter_input(blocks, input[1]) + MARGIN)
+    
+    return h
+
+
--- src/post-processing/main.py
+++ src/post-processing/main.py
@@ -1,12 +1,12 @@
-
 import zipfile
 import json
-import random
 import datetime
 import os
 import sys
 
-random.seed(0)
+import utils
+import clean_up_blocks
+
 
 ROOT_DIR = ''
 PATH_SOURCE_PROJECT = os.path.join(ROOT_DIR, 'Procedural Sandbox goboscript.sb3')
@@ -16,11 +16,6 @@ PATH_TARGET_PROJECT = os.path.join(ROOT_DIR, 'Procedural Sandbox.sb3')
 
 project_archive = zipfile.ZipFile(PATH_SOURCE_PROJECT, 'r')
 project_data: dict = json.loads(project_archive.read('project.json'))
-
-
-def serialize_project_json(project, indent=None):
-    return json.dumps(project, ensure_ascii=False, indent=indent, separators=(',', ':'))
-
 
 
 # reorder the sprites
@@ -41,20 +36,8 @@ for i, tgt in enumerate(project_data['targets']):
 
 # fix the stage size costume
 
-def get_costume_by_name(target: dict, name: str):
-    for costume in target.get('costumes', []):
-        if costume.get('name') == name:
-            return costume
-    return None # no costume found
-
-def get_target_by_name(project: dict, name: str):
-    for target in project.get('targets', []):
-        if target.get('name') == name:
-            return target
-    return None # no target found
-
-target = get_target_by_name(project_data, 'stage_size')
-costume = get_costume_by_name(target, 'probe')
+target = utils.get_target_by_name(project_data, 'stage_size')
+costume = utils.get_costume_by_name(target, 'probe')
 costume['bitmapResolution'] = 2
 costume['rotationCenterX'] = 2
 costume['rotationCenterY'] = 0
@@ -63,13 +46,7 @@ costume['rotationCenterY'] = 0
 
 # add the monitors
 
-def get_monitor_by_id(project: dict, monitor_id: str):
-    for monitor in project.get('monitors', []):
-        if monitor.get('id') == monitor_id:
-            return monitor
-    return None # no monitor found
-
-monitor = get_monitor_by_id(project_data, 'copy_this')
+monitor = utils.get_monitor_by_id(project_data, 'copy_this')
 if monitor is None:
     project_data['monitors'].append({
         "id": "copy_this",
@@ -89,44 +66,32 @@ if monitor is None:
 
 # add a comment to the thumbnail containing project info
 
-def random_id(prefix='', avoid=None):
-    if not isinstance(avoid, (dict, set, list)): avoid = {}
-    for _ in range(100):
-        temp = str(prefix) + "".join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=12))
-        if temp not in avoid: return temp
-    raise Exception('no vacant ids')
-
-def add_comment_to_target(target: dict, text, x=0, y=0, width=200, height=200, minimized=False, blockId=None):
-    if 'comments' not in target:
-        target['comments'] = {}
-    
-    target['comments'][random_id()] = {
-        'blockId': blockId,
-        'x': x,
-        'y': y,
-        'width': width,
-        'height': height,
-        'minimized': minimized,
-        'text': text,
-    }
-
-target = get_target_by_name(project_data, '_')
+target = utils.get_target_by_name(project_data, '_')
 
 try:
     gs_ver = project_data['meta']['agent'].removeprefix('goboscript v')
 except:
     gs_ver = '?'
 
-add_comment_to_target(target, "\n".join([
+utils.add_comment_to_target(target, "\n".join([
     'Procedural Sandbox',
     'Created by awesome-llama',
     '',
     '======',
     f'build_date: {datetime.datetime.now(datetime.timezone.utc)}',
-    f'approx_json_size: {round(len(serialize_project_json(project_data))/1000)/1000} MB',
+    f'approx_json_size: {round(len(utils.serialize_project_json(project_data))/1000)/1000} MB',
     f'gs_ver: {gs_ver}',
     f'py_ver: {sys.version_info.major}.{sys.version_info.minor}',
-]), x=300, width=500, height=600)
+]), x=500, width=500, height=600)
+
+
+
+# move the blocks so they don't overlap
+
+for target in project_data['targets']:
+    clean_up_blocks.clean_target(target)
+
+
 
 
 
@@ -140,7 +105,7 @@ with zipfile.ZipFile(PATH_TARGET_PROJECT, 'w') as new_archive:
     for item in project_archive.infolist():
         if item.filename == 'project.json':
             # Replace project.json with updated version
-            json_data = serialize_project_json(project_data)
+            json_data = utils.serialize_project_json(project_data)
             new_archive.writestr('project.json', json_data, zipfile.ZIP_DEFLATED)
         else:
             # Copy other files as-is
