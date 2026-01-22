60b2f5ed7358af9e58bbfa6065698a56b6436805
awesome-llama <17157128+awesome-llama@users.noreply.github.com>
2026-01-23 03:52:05 +1100

    refactor clean up blocks script

--- src/post-processing/clean_up_blocks.py
+++ src/post-processing/clean_up_blocks.py
@@ -1,10 +1,14 @@
-GRID_SIZE = 40 # the block grid is 40 units. Reporters are 40 units tall
-MARGIN = 8
+# Clean up the scripts like the Scratch editor's "clean up blocks" function.
 
-SCRIPT_SPACING = GRID_SIZE*2
+import copy
+
+GRID_SIZE = 40 # The block grid is 40 units. Reporters are 40 units tall.
+MARGIN = 8 # Additional space above+below a block's inputs (is still part of the block).
+
+SCRIPT_SPACING = GRID_SIZE*2 # How much space to leave between separate scripts.
 
 
-# some hats should be closer to the top than others for readability
+# Some hats should be closer to the top than others for readability:
 HAT_PRIORITY = [
     'event_whenflagclicked',
     'event_whenkeypressed',
@@ -15,8 +19,13 @@ HAT_PRIORITY = [
     'procedures_definition',
 ]
 
+# All blocks that are C shaped:
+C_SHAPED_OPCODES = {'control_repeat', 'control_repeat_until', 'control_while', 'control_for_each', 'control_forever', 'control_if', 'control_if_else', 'control_all_at_once'}
 
-def clean_target(target):
+
+def clean_target(target: dict):
+    """Rearrange all scripts in a target."""
+
     blocks = target['blocks']
 
     # find all the parent blocks and sort them
@@ -37,49 +46,65 @@ def clean_target(target):
         block = blocks[parent[0]]
         block['x'] = x
         block['y'] = y
-        y += (script_height(blocks, block_id)) + SCRIPT_SPACING
+        y += (_script_height(blocks, block_id)) + SCRIPT_SPACING
 
 
-def script_height(blocks, parent_id):
-    # recurse though children to find dimensions of each block
+
+def _script_height(blocks, parent_id):
+    """Find the height of an entire script by recursing though children."""
     
     h = _height_stack(blocks, parent_id)
     
     if blocks[parent_id]['opcode'] == 'procedures_definition':
-        h += MARGIN*2 # definition hats are tall
+        h += MARGIN * 2 # accounts for prototype stack block in definition
     
-    # h += 36 # definition
+    # h += 36 # definition top
     # h += 18 # rounded top part of hat
     
     return h
 
 
+
 def _height_stack(blocks, block_id):
+    """Returns the height of a vertical stack of blocks."""
+
+    if block_id is None: return 0
+
     block = blocks[block_id]
 
-    h = GRID_SIZE + MARGIN # stack height
+    h = GRID_SIZE + MARGIN # the base height of a stack block is grid size + margin above and below (it's larger than a reporter)
 
     if block['opcode'].startswith('pen_'):
-        h += 8 # pen blocks are slightly taller due to the icon
+        h += 8 # icons in extension blocks make them slightly taller
 
-    input_h = 0
-    nested_h = 0
-    for input_name, input in block.get('inputs', {}).items():
-        if input[0] == 2 and input[1] is not None:
+    inputs = copy.copy(block.get('inputs', {}))
+
+    # add missing inputs
+    if block['opcode'] in C_SHAPED_OPCODES and 'SUBSTACK' not in inputs:
+        inputs['SUBSTACK'] = [2, None]
+    if block['opcode'] == 'control_if_else' and 'SUBSTACK2' not in inputs:
+        inputs['SUBSTACK2'] = [2, None]
+
+    input_h = 0 # the input in the top part of the block such as repeat count
+    nested_h_total = 0 # total height of all nested stacks
+
+    for input_name, input in inputs.items():
+        if input[0] == 2:
             if input_name == 'SUBSTACK' or input_name == 'SUBSTACK2':
                 # stack
-                nested_h += _height_stack(blocks, input[1])
-                nested_h += 32 # bottom part of C shape
+                nested_h_total += max(24, _height_stack(blocks, input[1])) # empty slot or nested script
+                nested_h_total += 32 # bottom part of C shape
+
             else:
                 # boolean
                 input_h = max(input_h, _height_reporter_input(blocks, input[1]) + MARGIN)
-            
-        elif input[0] == 3 and input[1] is not None:
+
+        elif input[0] == 3:
             # round reporter
             input_h = max(input_h, _height_reporter_input(blocks, input[1]) + MARGIN)
-    
+
     h = max(h, input_h)
-    h += nested_h
+    h += nested_h_total
     
     # next stack block
     if block['next'] is not None:
@@ -90,10 +115,12 @@ def _height_stack(blocks, block_id):
 
 
 def _height_reporter_input(blocks, block_id):
-    if block_id is None: 
-        return 0
+    """Returns the height of a reporter."""
+
+    if block_id is None: return 0
+
     if not isinstance(block_id, str):
-        return GRID_SIZE
+        return GRID_SIZE # input not occupied by block
     
     block = blocks[block_id]
 
